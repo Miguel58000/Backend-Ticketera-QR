e72874db65d5354ea8fca20e961f0f77dec6138f
NicolasDiDio <NicolasDiDio09@gmail.com>
2025-02-10 13:25:21 -0300

    ticket & tipoTicket CRUDs added

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -5,6 +5,8 @@ import clienteRouter from "./cliente.routes.js";
 import politicaRouter from "./politica.routes.js";
 import categoriaRouter from "./categoria.routes.js";
 import eventoRouter from './evento.routes.js';
+import tipoTicketRouter from './tipoTicket.routes.js';
+import ticketRouter from './ticket.routes.js';
 
 const router = Router();
 
@@ -12,6 +14,8 @@ router.use("/organizaciones", organizacionRouter);
 router.use("/clientes", clienteRouter);
 router.use("/politicas", politicaRouter);
 router.use("/categorias", categoriaRouter);
-router.use("/eventos", eventoRouter)
+router.use("/eventos", eventoRouter);
+router.use("/tipoTickets", tipoTicketRouter);
+router.use("/tickets", ticketRouter);
 
 export default router;
